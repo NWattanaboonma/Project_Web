f9868ee45255805c4fa0e8503d5ecbe9c68c331a
Kmookk <157203627+Kmookk@users.noreply.github.com>
2024-04-18 16:45:53 +0700

    Register.js

--- testing/Front_end/Register/Register.js
+++ testing/Front_end/Register/Register.js
@@ -21,4 +21,19 @@ connection.connect(function(err){
   console.log(`Connected DB: ${process.env.DB_name}`);
 });
 
+function getResponses() {
+    let query = document.getElementById("name").value;
+    let rount = "https://api.spoonacular.com/recipes/complexSearch?apiKey="+"548b3572a843413cae59a2d5919afde6"+"&query="+query;
+    
+    fetch(rount)
+    .then((res)=> res.json())
+    .then((data)=>{
+    console.log(data);
+    document.querySelector("ul").innerHTML=" ";
+    for (let i=0;i<data.number;i++){
+        document.querySelector("ul").innerHTML += "<li><b>"+data.results[i].title+":</b></li>" + '<img src="'+data.results[i].image+'">';
+    }
+    })
+    }
+
 
