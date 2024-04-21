fcbac95367a387390ee921b53619cf5eff60159d
WPornpatchara <pornpatcharaw@gmail.com>
2024-04-22 00:40:09 +0700

    fa

--- testing/Front_end/List/user_list.js
+++ testing/Front_end/List/user_list.js
@@ -29,7 +29,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
-
 function updateuser() {
     // Get user information from input fields
     const email = localStorage.getItem("User_id")
