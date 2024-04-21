48cdf7e68f88b97ef383b43af82fe665d3226087
NWattanaboonma <eakann2004@gmail.com>
2024-04-21 14:13:20 +0700

    Done_List_admin

--- testing/Front_end/List/List_admin.js
+++ testing/Front_end/List/List_admin.js
@@ -1,31 +1,35 @@
-function
-fetch('http://localhost:2021/List_Admin', {
+function using(){
+  fetch('http://localhost:2021/List_Admin', {
     method: 'GET',
     headers: {
-      'Content-Type': 'application/json'
-    },})
-  .then(result => result.json())
-  .then(data => {
-    console.log(data)
-    const box = document.getElementById('box');
-    box.innerHTML="" //clear
-    for(var i=0;i<data.length;i++){
-    const adminData = response.data[0];
-    const adminId = adminData.AdminID;
-    const userName = adminData.UserName;
-    const roles = adminData.Roles;
-    const li = document.createElement('li');
-    li.innerHTML = `
-      <div id="number">${i}</div>
+        'Content-Type': 'application/json'
+    },
+})
+.then(result => result.json())
+.then(data => {
+    console.log(data); 
+    const infro = data.data;
+    console.log(infro);
+    for (var i=0;i<infro.length;i++){
+      const List=infro[i]
+      const email=List.AdEmail
+      const ID = List.AdminID
+      const roles= List.Roles
+      const userName= List.UserName
+      document.getElementById("list").innerHTML+=`
+      <div class='box'>
+      <div id="number">${i+1}</div>
       <div id="Admin_data">
-        <p>${userName}(${adminId}) Role:${roles}</p>
+        <p>${userName}(${ID}) Role:${roles} email: ${email}</p>
       </div>
       <button id="Detail" onclick="updateadd()">Detail</button>
+      </div>
     `;
-    box.appendChild(li);
     }
-    
-  })
-  .catch(error => {
-    console.error('Error registering user:', error); 
-  });
+})
+.catch(error => {
+    console.error('Error fetching data:', error); 
+});
+
+}
+
