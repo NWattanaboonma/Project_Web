adf41edf3b539cc95eccd6a288bbf8a422aaeb3d
Kmookk <157203627+Kmookk@users.noreply.github.com>
2024-04-21 23:09:46 +0700

    mook

--- testing/Front_end/List/user_list.js
+++ testing/Front_end/List/user_list.js
@@ -17,9 +17,8 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log(data); 
         const infro = data.data;
 
-        console.log(infro.Phone)
-        document.getElementById("newFirstName").value=infro.Fname
-        document.getElementById("newLastName").value=infro.Lname
+        document.getElementById("newFirstName").value=infro.FName
+        document.getElementById("newLastName").value=infro.LName
         document.getElementById("newEmail").value=infro.UserEmail
         document.getElementById("newAge").value=infro.Age
         document.getElementById("newPhoneNumber").value=infro.Phone
@@ -31,5 +30,45 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+function updateUser() {
+    // Get user information from input fields
+    const email = localStorage.getItem("User_id")
+    const firstName = document.getElementById("newFirstName").value;
+    const lastName = document.getElementById("newLastName").value;
+   
+    const age = document.getElementById("newAge").value;
+    const phone = document.getElementById("newPhoneNumber").value;
+ // const email = document.getElementById("newEmail").value;
+    const userData = {
+        "firstName": firstName,
+        "lastName": lastName,
+        "age": age,
+        "phoneNumber": phone
+    };
 
+    fetch("/updateUser", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email: email, // Pass user email for identification
+            userData: userData // Pass user data for update
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error("Failed to update user information");
+    })
+    .then(data => {
+        // Handle successful update (e.g., display success message)
+        console.log("User information updated successfully:", data);
+    })
+    .catch(error => {
+        // Handle error (e.g., display error message)
+        console.error("Error updating user information:", error);
+    });
+}
 
