da82be025d810ff9e72f3e4b9137c7d6295fcf32
NWattanaboonma <eakann2004@gmail.com>
2024-04-21 15:02:29 +0700

    No_going

--- testing/Front_end/hosting.js
+++ testing/Front_end/hosting.js
@@ -68,8 +68,8 @@ call('Edit','Edit.html','/Edit');
 call('List','list_user.html','/List_User')
 call('List','List_admin.html','/List_Admin')
 call('List','Admin_detail.html','/Admin_detail')
-call('List','user_list','/user_list')
-call('List','user_list','/user_list')
+call('List','user_list.html','/user_list')
+// call('List','user_list','/user_list')
 // Project_web.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'Home_Page_Website', 'Home_page.html'));});
 
