7a49f9a7c5a6a7f5c77b251808bc6140711ab2d5
NWattanaboonma <eakann2004@gmail.com>
2024-04-22 02:40:39 +0700

    Most done_Search

--- testing/Front_end/Adv_search/Search_showproducts.js
+++ testing/Front_end/Adv_search/Search_showproducts.js
@@ -3,37 +3,46 @@ document.addEventListener("DOMContentLoaded", function() {
     const Data = localStorage.getItem("Search_Data");
     console.log(Data)
     
-fetch('http://localhost:2021/List_User', {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    })
-    .then(result => result.json())
-    .then(data => {
-        console.log(data); 
-        const infro = data.data;
-        console.log(infro);
-        for (var i=0;i<infro.length;i++){
-        const List=infro[i]
-        const firstname=List.FName
-        const lastname=List.LName
-        const email=List.UserEmail
-        const Call = List.Phone
-        const Age= List.Age
-        const ID = String("'"+email+"'")
-        //   const Bt= List.Birthdate
-        document.getElementById("list").innerHTML+=`
-        <div class='box'>
-        <div id="number">${i+1}</div>
-        <div id="User_Data">
-            <p>${firstname} ${lastname} (Contact: <span class="color_it">${Call}</span>) Age:${Age} email: <a href = "mailto: ${email}" >${email}</a></p>
-        </div>
-        <button id="Detail" onclick="updateuser(${ID})">Detail</button>
-        </div>
-        `;
-        }
-    })
+    fetch('http://localhost:2021/List_Product', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        })
+        .then(result => result.json())
+        .then(data => {
+            // console.log(data); 
+            const infro = data.data;
+            var Check=true;
+            // console.log(infro);
+            for (var i=0;i<infro.length;i++){
+                const List=infro[i]
+                const Name = List.ProductName
+                const Collection = List.Collection
+                const Color= List.Color
+
+                if(Data===Name){
+                    Check=false;
+                    console.log("Name")
+                }else if(Data===Collection){
+                    Check=false;
+                    console.log("Collection")
+                }else if(Data===Color){
+                    Check=false;
+                    console.log("Color")
+                }
+            }
+            if(Check){
+                console.log("in")
+                for (var i=0;i<infro.length;i++){
+                    // const List=infro[i]
+                    // const Name = List.ProductName
+                    // const Collection = List.Collection
+                    // const Color= List.Color
+                    console.log("working");
+                };
+            }
+        })
     .catch(error => {
         console.error('Error fetching data:', error); 
     });
