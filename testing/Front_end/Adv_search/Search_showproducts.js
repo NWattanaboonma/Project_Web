46e41d8f4fb87fa8d3d423ceee31eeff7e6ccc4d
NWattanaboonma <eakann2004@gmail.com>
2024-04-22 14:10:03 +0700

    ya

--- testing/Front_end/Adv_search/Search_showproducts.js
+++ testing/Front_end/Adv_search/Search_showproducts.js
@@ -25,32 +25,58 @@ document.addEventListener("DOMContentLoaded", function() {
                 const price=List.Price
                 if(Data===Name){
                     Check=false;
-                    document.getElementById("store").innerHTML=`
+                    document.getElementById("store").innerHTML+=`
                     <div class="textbox">
                     <img src=${img} class="imgproducts">
                     <div class="detail">
-                        <h3><b>${Name}<br>(graphic t-shirt)</b></h3>
-                        <h3>Black</h3>
-                        <h3>790 Baht</h3>
-                        <button class="buy">Buy</button>
+                        <h3><b>${Name}<br>${mt}</b></h3>
+                        <h3>${Color}</h3>
+                        <h3>${price} Baht</h3>
                     </div>
                     </div>`;
                 }else if(Data===Collection){
                     Check=false;
-                    console.log("Collection")
+                    document.getElementById("store").innerHTML+=`
+                    <div class="textbox">
+                    <img src=${img} class="imgproducts">
+                    <div class="detail">
+                        <h3><b>${Name}<br>${mt}</b></h3>
+                        <h3>${Color}</h3>
+                        <h3>${price} Baht</h3>
+                    </div>
+                    </div>`;
                 }else if(Data===Color){
                     Check=false;
-                    console.log("Color")
+                    document.getElementById("store").innerHTML+=`
+                    <div class="textbox">
+                    <img src=${img} class="imgproducts">
+                    <div class="detail">
+                        <h3><b>${Name}<br>${mt}</b></h3>
+                        <h3>${Color}</h3>
+                        <h3>${price} Baht</h3>
+                    </div>
+                    </div>`;
                 }
             }
             if(Check){
                 console.log("in")
                 for (var i=0;i<infro.length;i++){
-                    // const List=infro[i]
-                    // const Name = List.ProductName
-                    // const Collection = List.Collection
-                    // const Color= List.Color
-                    console.log("working");
+                    const List=infro[i]
+                    const Name = List.ProductName
+                    const Collection = List.Collection
+                    const Color= List.Color
+                    const img=List.Image
+                    const mt=List.Meterial
+                    const price=List.Price
+                    document.getElementById("store").innerHTML+=`
+                    <div class="textbox">
+                    <img src=${img} class="imgproducts">
+                    <div class="detail">
+                        <h3><b>${Name}<br>${mt}</b></h3>
+                        <h3>${Color}</h3>
+                        <h3>${price} Baht</h3>
+                    </div>
+                    </div>`;
                 };
             }
         })
