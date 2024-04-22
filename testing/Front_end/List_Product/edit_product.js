7c3a59bdc8875af5fc0991e26c4df2a793790176
khamomile45 <109598575+khamomile45@users.noreply.github.com>
2024-04-22 22:15:14 +0700

    update&deleteproduct

--- testing/Front_end/List_Product/edit_product.js
+++ testing/Front_end/List_Product/edit_product.js
@@ -25,5 +25,67 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+function updateproduct() {
+    // Get user information from input fields
+    const id = localStorage.getItem("edit")
+    const productName = document.getElementById("productName").value;
+    const collection = document.getElementById("collectionName").value;
+   
+    const color = document.getElementById("productColor").value;
+    const price = document.getElementById("productPrice").value;
+    
+    const productData = {
+        "ProductName": productName,
+        "Collection": collection,
+        "Color": color,
+        "Price": price
+    };
+
+    fetch("http://localhost:2021/updateProduct", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: id, // Pass user email for identification
+            productData: productData // Pass user data for update
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error("Failed to update user information");
+    })
+    .then(data => {
+        // Handle successful update (e.g., display success message)
+        // console.log("User information updated successfully:", data);
+        window.location.href='/List_product'; 
+    })
+    .catch(error => {
+        // Handle error (e.g., display error message)
+        console.error("Error updating user information:", error);
+    });
+}
+
+function deleteproduct() {
+    // Get user information from input fields
+    const id = localStorage.getItem("edit")
 
 
+    fetch('http://localhost:2021/deleteProduct', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: id})
+    })
+    .then(result => result.json())
+    .then(data => {
+        console.log(data)
+        window.location.href='/List_Product'; 
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error); 
+    });
+}
